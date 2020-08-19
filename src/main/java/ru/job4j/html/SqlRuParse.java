890eed9402fbc142d39218705db618427c9b1743
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-08-19 12:09:14 +0300

    Парсинг HTML страницы. [#265659] review fix

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,14 +6,17 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class SqlRuParse {
+    private static final Integer LINK_TD = 1;
+    private static final Integer DATE_TD = 5;
+
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements trs = doc.select(".forumTable tr");
         for (Element row : trs) {
 
-            System.out.println(row.child(1).text());
-            System.out.println(row.child(1).attr("href"));
-            System.out.println(row.child(5).text());
+            System.out.println(row.child(LINK_TD).text());
+            System.out.println(row.child(LINK_TD).attr("href"));
+            System.out.println(row.child(DATE_TD).text());
 
         }
     }
