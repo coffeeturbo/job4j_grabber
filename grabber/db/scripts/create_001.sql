4591f0d60baf0976a5094903eee1d00f971d8234
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-08-24 15:33:29 +0300

    База данных для Grabber [#265662] review fix

--- grabber/db/scripts/create_001.sql
+++ grabber/db/scripts/create_001.sql
@@ -1,7 +1,7 @@
-
-create table rabbit (
+create table post (
     id serial primary key not null,
     name varchar(500) NOT NULL ,
+    text varchar(1000) NOT NULL,
     link varchar(1000) UNIQUE NOT NULL,
     created_at date NOT NULL
 );
