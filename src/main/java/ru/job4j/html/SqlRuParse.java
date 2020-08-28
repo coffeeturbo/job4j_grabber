89e94b2c1cb98127b9e4e7babcd9a0777d7e52e6
CoffeeTurbo@gmail.com <coffeeturbo@gmail.com>
2020-08-28 22:10:52 +0300

    Преобразование даты [#348016]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,9 +5,14 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class SqlRuParse {
     private static final Integer LINK_TD = 1;
     private static final Integer DATE_TD = 5;
+
 
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
@@ -16,8 +21,34 @@ public class SqlRuParse {
 
             System.out.println(row.child(LINK_TD).text());
             System.out.println(row.child(LINK_TD).attr("href"));
-            System.out.println(row.child(DATE_TD).text());
 
+            String date = row.child(DATE_TD).text();
+            Date dt = parseDate(date);
+
+            System.out.println(date + " = " + dt);
         }
+
+    }
+
+    private static Date parseDate(String date) {
+        Date parsedDate = null;
+        String[] splitDate = date.split(" ");
+
+        if (splitDate.length > 1) {
+            splitDate[1] = splitDate[1].concat(".");
+
+            String concat = String.join(" ", splitDate);
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd LLL yy, HH:mm", Locale.getDefault());
+
+            try {
+                parsedDate = formatter.parse(concat);
+            } catch (ParseException e) {
+            }
+        }
+
+
+
+        return parsedDate;
     }
 }
